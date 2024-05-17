e55c6cc710db4945ac8de898669c92472526dc11
Dinislam Zalimkhanov <Zalimkhanova04@mail.ru>
2024-05-17 21:54:48 +0300

    server patching

--- device_chat/consumers.py
+++ device_chat/consumers.py
@@ -10,6 +10,27 @@ class DeviceConsumer(AsyncWebsocketConsumer):
         self.device_id = self.scope['url_route']['kwargs']['device_id']
         self.room_group_name = f'device_{self.device_id}'
 
+        try:
+            device = await self.get_device(self.device_id)
+        except:
+            await self.accept()
+            await self.send(text_data=json.dumps({
+                'type': 'error',
+                'message': 'Device is not exist'
+            }))
+            await self.close()
+            return
+
+        # Проверяем, что устройство подключено
+        if not device.is_connected:
+            await self.accept()
+            await self.send(text_data=json.dumps({
+                'type': 'error',
+                'message': 'Device is not connected'
+            }))
+            await self.close()
+            return
+
         # Присоединяемся к группе
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -19,8 +40,10 @@ class DeviceConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
         # Отправляем текущее состояние новому пользователю
-        device = await self.get_or_create_device(self.device_id)
+        devices = await self.get_all_devices()
         await self.send(text_data=json.dumps({
+            'type': 'device_state',
+            'name': device.name,
             'image': device.image,
             'brightness': device.brightness,
             'is_on': device.is_on
@@ -37,7 +60,7 @@ class DeviceConsumer(AsyncWebsocketConsumer):
         try:
             data = json.loads(text_data)
             # Проверка валидности данных может быть добавлена здесь
-            if not ('image' in data and 'brightness' in data and 'is_on' in data):
+            if not ('name' in data and 'image' in data and 'brightness' in data and 'is_on' in data):
                 raise ValueError("Invalid data format")
 
             # Обновляем состояние в базе данных
@@ -48,21 +71,103 @@ class DeviceConsumer(AsyncWebsocketConsumer):
                 self.room_group_name,
                 {
                     'type': 'device_message',
+                    'name': device.name,
                     'image': device.image,
                     'brightness': device.brightness,
                     'is_on': device.is_on
                 }
             )
-        except json.JSONDecodeError as e:
-            await self.send(text_data=json.dumps({'error': 'Invalid JSON data'}))
+        except json.JSONDecodeError:
+            await self.send(text_data=json.dumps({'type': 'error', 'message': 'Invalid JSON data'}))
         except ValueError as e:
-            await self.send(text_data=json.dumps({'error': str(e)}))
-        except Exception as e:
-            await self.send(text_data=json.dumps({'error': 'An unexpected error occurred'}))
+            await self.send(text_data=json.dumps({'type': 'error', 'message': str(e)}))
+        except Exception:
+            await self.send(text_data=json.dumps({'type': 'error', 'message': 'An unexpected error occurred'}))
 
     async def device_message(self, event):
         # Отправка сообщения обратно в WebSocket
         await self.send(text_data=json.dumps({
+            'type': 'device_message',
+            'name': event['name'],
+            'image': event['image'],
+            'brightness': event['brightness'],
+            'is_on': event['is_on']
+        }))
+
+    @database_sync_to_async
+    def is_device_connected(self, device_id):
+        try:
+            device = Device.objects.get(device_id=device_id)
+            return device.is_connected
+        except Device.DoesNotExist:
+            return False
+
+    @database_sync_to_async
+    def get_device(self, device_id):
+        device = Device.objects.get(device_id = device_id)
+        return device
+
+    @database_sync_to_async
+    def update_or_create_device(self, data):
+        device, _ = Device.objects.update_or_create(
+            device_id=self.device_id,
+            defaults={
+                'name': data['name'],
+                'image': data['image'],
+                'brightness': data['brightness'],
+                'is_on': data['is_on']
+            }
+        )
+        return device
+
+    @database_sync_to_async
+    def get_all_devices(self):
+        devices = Device.objects.all()
+        return [{'device_id': device.device_id, 'name': device.name, 'is_connected': device.is_connected} for device in
+                devices]
+
+
+class DeviceStatusConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.device_id = self.scope['url_route']['kwargs']['device_id']
+        self.room_group_name = f'device_{self.device_id}'
+
+        # Присоединяемся к группе
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        device = await self.get_or_create_device(self.device_id)
+        await self.accept()
+        await self.send(text_data=json.dumps({
+            'type': 'device_state',
+            'device': {
+                'name': device.name,
+                'image': device.image,
+                'brightness': device.brightness,
+                'is_on': device.is_on
+            }
+        }))
+
+        # Обновляем статус подключения устройства
+        await self.update_device_status(True)
+
+    async def disconnect(self, close_code):
+        # Отсоединяемся от группы
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        # Обновляем статус подключения устройства
+        await self.update_device_status(False)
+
+    async def device_message(self, event):
+        # Обработка сообщения от другого консьюмера
+        await self.send(text_data=json.dumps({
+            'type': 'device_message',
+            'name': event['name'],
             'image': event['image'],
             'brightness': event['brightness'],
             'is_on': event['is_on']
@@ -73,6 +178,7 @@ class DeviceConsumer(AsyncWebsocketConsumer):
         device, created = Device.objects.get_or_create(
             device_id=device_id,
             defaults={
+                'name': f'device-{device_id}',
                 'image': [[0] * 16 for _ in range(16)],  # Пустой рисунок 16x16
                 'brightness': 100,
                 'is_on': True
@@ -81,13 +187,5 @@ class DeviceConsumer(AsyncWebsocketConsumer):
         return device
 
     @database_sync_to_async
-    def update_or_create_device(self, data):
-        device, _ = Device.objects.update_or_create(
-            device_id=self.device_id,
-            defaults={
-                'image': data['image'],
-                'brightness': data['brightness'],
-                'is_on': data['is_on']
-            }
-        )
-        return device
+    def update_device_status(self, is_connected):
+        Device.objects.filter(device_id=self.device_id).update(is_connected=is_connected)
--- device_chat/routing.py
+++ device_chat/routing.py
@@ -3,4 +3,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/device/(?P<device_id>\w+)/$', consumers.DeviceConsumer.as_asgi()),
+    re_path(r'ws/device_status/(?P<device_id>\w+)/$', consumers.DeviceStatusConsumer.as_asgi()),
 ]
